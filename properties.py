5fdd02c18b5e1b58a61d27142624211f81c34eb7
KirilStrezikozin <kirilstrezikozin@gmail.com>
2022-11-30 21:09:41 +0200

    pref: mask color props size set to 4

--- properties.py
+++ properties.py
@@ -1537,8 +1537,8 @@ class BM_Map(bpy.types.PropertyGroup):
     map_mask_color1 : bpy.props.FloatVectorProperty(
         name="Color1",
         description="What color to use as Color1 for masking",
-        default=(0, 0, 0),
-        size=3,
+        default=(0, 0, 0, 1),
+        size=4,
         min=0,
         max=1,
         precision=3,
@@ -1548,8 +1548,8 @@ class BM_Map(bpy.types.PropertyGroup):
     map_mask_color2 : bpy.props.FloatVectorProperty(
         name="Color2",
         description="What color to use as Color2 for masking",
-        default=(1, 1, 1),
-        size=3,
+        default=(1, 1, 1, 1),
+        size=4,
         min=0,
         max=1,
         precision=3,
