3abb77c64f21b10f2c3ae05b365655e9cd73568e
KirilStrezikozin <kirilstrezikozin@gmail.com>
2022-12-01 12:38:39 +0200

    add: id map colors generation
    hue, grayscale, random

--- utils.py
+++ utils.py
@@ -2252,6 +2252,117 @@ def BM_MAP_PROPS_MapPreview_CustomNodes_Update(context, map_tag):
     }
     
     map = BM_Map_Get(object_item)
+
+    # for id, loop over materials with COLORI in the name
+    # because need to calculate size of color stepping
+    if map_tag == "ID":
+        import colorsys
+        
+        for object in objects:
+            color_mats = []
+            for material in object.data.materials:
+                if material is None:
+                    continue
+                if material.name.find("BM_CustomMaterial_") != -1 and material.name.find("COLOR") != -1:
+                    color_mats.append(material) 
+            if len(color_mats) == 0:
+                continue
+            
+            # getting colors
+            colors = []
+            if map.map_matid_algorithm == 'GRAYSCALE':
+                step = round(1 / (len(color_mats) - 1), 3)
+                color = [0.0, 0.0, 1.0]
+                for i in range(len(color_mats)):
+                    rgb = list(colorsys.hsv_to_rgb(color[0], color[1], color[2]))
+                    rgb.append(1.0)
+                    colors.append(tuple(rgb))
+                    color[2] -= step
+            
+            if map.map_matid_algorithm == 'HUE':
+                step = round(1 / (len(color_mats)), 3)
+                color = [1.0, 1.0, 1.0]
+                for i in range(len(color_mats)):
+                    rgb = list(colorsys.hsv_to_rgb(color[0], color[1], color[2]))
+                    rgb.append(1.0)
+                    colors.append(tuple(rgb))
+                    color[0] -= step
+            
+            if map.map_matid_algorithm == 'RANDOM':
+                # (dev) debug .prefs/rgb_color_scatter.py to see how this works
+                # given variables
+                Points = len(color_mats) # number of points to scatter aka colors to get, > 0
+                SOrbit = 4 # value indicating at what number of scattered points the first Saturation orbit should end, > 0
+                VOrbit = 24 # value indicating at what number of scattered points the first Value orbit should end, > 0
+                MinOrbit = 2 # minimum on SOrbit = SOrbit / MinOrbit, same for minimum on VOrbit, float > 0
+
+                def get_orbit_size(orbit_capacity, points):
+                    n = 1
+                    new_points = [1]
+                    points_cache = 1
+                    while points_cache < points:
+                        # minimum n of points on the current V orbit
+                        minimum = int(orbit_capacity / MinOrbit)
+                        # how many points left in total
+                        points_left = points - points_cache
+                        can_contain = points_left - ((points_left - orbit_capacity) + minimum)
+                        # if left more than orbit can contain, add orbit_capacity
+                        if points_left - orbit_capacity >= orbit_capacity:
+                            points_cache += orbit_capacity
+                            new_points.append(orbit_capacity)
+                        elif can_contain >= minimum and can_contain <= points_left:
+                            points_cache += can_contain
+                            new_points.append(can_contain)
+                        else:
+                            points_cache += points_left
+                            new_points.append(points_left)
+                        orbit_capacity *= 2
+                        n += 1
+                    return n, sorted(new_points)
+
+                n_v, v_points = get_orbit_size(VOrbit, Points)
+
+                if n_v - 1 == 0:
+                    v_step = 0
+                else:
+                    v_step = round(1 / (n_v - 1), 3)
+
+                color = [1.0, 0.0, 0.0]
+                for v_orbit_size in v_points:
+                    color[1] = 0.0
+                    n_s, s_points = get_orbit_size(SOrbit, v_orbit_size)
+
+                    if n_s - 1 == 0:
+                        s_step = 0
+                    else:
+                        s_step = round(1 / (n_s - 1), 3)
+
+                    for s_orbit_size in s_points:
+                        color[0] = 1.0
+                        if s_orbit_size - 1 == 0:
+                            h_step = 0
+                        else:
+                            h_step = round(1 / s_orbit_size, 3)
+
+                        for i in range(s_orbit_size):
+                            rgb = list(colorsys.hsv_to_rgb(color[0], color[1], color[2]))
+                            rgb.append(1.0)
+                            colors.append(tuple(rgb))
+                            color[0] -= h_step
+                        
+                        color[1] += s_step
+                    
+                    color[2] += v_step
+            
+            # loop through needed materials
+            for mat_index, material in enumerate(color_mats):
+                material.use_nodes = True
+                nodes = material.node_tree.nodes
+                map_nodes = nodes_names_data[map_tag]
+                nodes[map_nodes[0]].inputs[0].default_value = colors[mat_index]
+
+        return
+
     # looping through materials
     for object in objects:
         for material in object.data.materials:
