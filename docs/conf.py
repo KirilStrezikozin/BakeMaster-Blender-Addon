d787c410c01b5783be655f01bcb8af0037b16477
KirilStrezikozin <kirilstrezikozin@gmail.com>
2022-12-19 14:47:24 +0200

    error: build not correct theme

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ release = bakemaster_version
 # ones.
 extensions = [
     # furo theme
-    # "python_sphinx_furo",
+    "python_sphinx_furo",
     # Sphinx's own extensions
     "sphinx.ext.autodoc",
     "sphinx.ext.extlinks",
@@ -63,7 +63,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "furo"
+html_theme = "python_sphinx_furo"
 html_title = "BakeMaster"
 language = "en"
 
