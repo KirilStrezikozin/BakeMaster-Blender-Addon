1f7452f8ea8a051104f373ee998391314ada1d65
KirilStrezikozin <kirilstrezikozin@gmail.com>
2023-03-13 18:19:39 +0200

    pref: fix layout.emboss_ attribute

--- addon/presets.py
+++ addon/presets.py
@@ -1183,7 +1183,7 @@ class BM_PresetPanel:
 
     @classmethod
     def draw_panel_header(cls, layout):
-        layout.embos_ = 'NONE'
+        layout.embos = 'NONE'
         layout.popover(
             panel=cls.__name__,
             icon='PRESET',
